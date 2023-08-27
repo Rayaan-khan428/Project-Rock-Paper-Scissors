ced9ec0ae0435f3575e3d02bad7d942e70fdb5ce
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-08-27 19:22:20 -0400

    code for styling elements

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,13 @@
 // DOM Manipulation
+
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// console.log(title);
+
+const listItems = document.querySelectorAll('.list-items');
+
+for (i=0; i<listItems.length; i++) {
+    listItems[i].style.fontSize = '2rem';
+}
+
+console.log(listItems);
