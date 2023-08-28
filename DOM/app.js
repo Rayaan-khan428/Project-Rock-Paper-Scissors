5d1aff486e10c9958faa51045d47c55cfaccdbc3
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-08-27 21:40:33 -0400

    adding attributes to new elements we create

--- DOM/app.js
+++ DOM/app.js
@@ -4,10 +4,26 @@
 // title.style.color = 'red';
 // console.log(title);
 
-const listItems = document.querySelectorAll('.list-items');
+// const listItems = document.querySelectorAll('.list-items');
 
-for (i=0; i<listItems.length; i++) {
-    listItems[i].style.fontSize = '2rem';
-}
+// for (i=0; i<listItems.length; i++) {
+//     listItems[i].style.fontSize = '2rem';
+// }
 
-console.log(listItems);
+// console.log(listItems);
+
+// Creating Elements ---------- //
+
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+// Adding Elements
+ul.append(li);
+
+// Modifying the text
+li.innerText = 'X-men';
+
+// modifying attribute/class
+
+li.setAttribute('id', 'main-heading');
+li.removeAttribute('id');
